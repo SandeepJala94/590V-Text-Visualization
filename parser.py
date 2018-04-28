3b3a9e25677c975c3b8d4220058b8decc76b4801
Sandeep Jala <jalasandeep914@gmail.com>
2018-04-27 20:27:23 -0400

    improved parsing

--- parser.py
+++ parser.py
@@ -1,12 +1,45 @@
 import re
 import csv
 
-def removeEndingNonLetter(word):
-	done = 0
-	# while(done == false)
-	# 	if(word[len(word)]
+def seriesOfReplacements(myLine):
+	tempLine = myLine
+	tempLine = tempLine.replace(",", " ")
+	tempLine = tempLine.replace("-"," ")
+	tempLine = tempLine.replace("."," ")
+	tempLine = tempLine.replace("!", " ")
+	tempLine = tempLine.replace(":", " ")
+	tempLine = tempLine.replace(";", " ")
+	tempLine = tempLine.replace("?", " ")
+	return tempLine
 
-	return word.lower()
+def isLetter(myChar):
+	if myChar == " ":
+		return 1
+
+	if myChar == "":
+		return 1
+
+	if myChar == "\n":
+		return 1
+
+	if ord(myChar) >= 97:
+		if ord(myChar) <= 122:
+			return 1
+
+	return 0
+
+def removeEndingNonLetter(myWord):
+	done = 0
+	word = myWord.lower()
+	while(done == 0):
+		i = len(word)
+		print("Inside removeEndingNonLetter -->", word, "<---")
+		if isLetter(word[i-1])==0:
+			word = word[:-1]
+		else:#isLetter(word[i-1])==1
+			done = 1
+
+	return word
 
 def printTextFileByParagraph(myList):
 	paragraphCount = 1
@@ -21,24 +54,28 @@ def printDict(myDict):
 		lineCount = lineCount+1
 
 
-
 f = open("PsychologyOfLinguisticForm.txt","r")
 myList = []
 for line in f:
     myList.append(line)
 
-printTextFileByParagraph(myList)
+#printTextFileByParagraph(myList)
 
 wordFreDict = {}
+lineCount = 1
 for line in myList:
-	for word in re.split(' ', line):
-		newWord = removeEndingNonLetter(word)
+	tempLine = seriesOfReplacements(line)
+	print(lineCount, ") ", line)
+	print(lineCount, ") ", line)
+	lineCount = lineCount+1
+	for word in tempLine.split():
+		#tempWord = seriesOfReplacements(word)
+		#newWord = removeEndingNonLetter(temp)
+		newWord = word.lower()
 		if newWord in wordFreDict.keys():
 			wordFreDict[newWord] = wordFreDict[newWord] + 1
 		else:
 			wordFreDict[newWord] = 1
-
-printDict(wordFreDict)
 
 # with open('dict.csv', 'wb') as csv_file:
 #     writer = csv.writer(csv_file)
